9fe582e15a29ee37fbd6152c0521e2083b9db6e1
Samuel Chorvat <sxc1101@student.bham.ac.uk>
2018-03-01 22:47:55 +0000

    Started work on JobSelection

--- src-pc/jobs/JobSelection.java
+++ src-pc/jobs/JobSelection.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class JobSelection {
+	
+	JobInput jb = new JobInput();
+	
+	
+	private ArrayList<Integer> orderedOrders;
+	private HashMap<Integer, ArrayList<String>> availableOrders = jb.getAvailableOrders();
+	private HashMap<String, ArrayList<Float>> itemRewardsWeights = jb.getItemRewardsWeights();
+	
+	private TrainingAlgorithm tA = new TrainingAlgorithm(availableOrders, itemRewardsWeights);
+	
+	private HashMap<String, Double> betaValuesFromTrainingSet = tA.train();
+	
+	public ArrayList<Integer> prioritize() {
+		
+		
+		
+		
+		return orderedOrders;				
+	}
+	
+	
+	
 
 }
