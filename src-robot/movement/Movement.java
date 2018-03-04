5287b1264949294d9b759fe8c383eca019e8636d
Brett James Saunders <bjs730@cs.bham.ac.uk>
2018-03-04 10:01:22 +0000

    Added more actions so they can be sent as commands, worked on movement

--- src-robot/movement/Movement.java
+++ src-robot/movement/Movement.java
@@ -4,15 +4,18 @@ import lejos.util.Delay;
 import rp.systems.WheeledRobotSystem;
 
 public class Movement {
+	private final int adjustmentValue;
 	private  final LightSensor LEFT_SENSOR;
 	private final LightSensor RIGHT_SENSOR;
 	private final DifferentialPilot PILOT;
+	private final int CORRECTION = 7;
 	private Configuration c = new Configuration();
 	
-	public Movement() {
+	public Movement(int _adjustmentValue) {
 		PILOT = new WheeledRobotSystem(Configuration.CUSTOM_EXPRESS_BOT).getPilot();
 		LEFT_SENSOR = new LightSensor(Configuration.LEFT_LIGHT_SENSOR);
 		RIGHT_SENSOR = new LightSensor(Configuration.RIGHT_LIGHT_SENSOR);
+		adjustmentValue = _adjustmentValue;
 	}
 	
 	public void nextAction(Action a) {
@@ -29,12 +32,26 @@ public class Movement {
 			}
 			PILOT.rotate(10);
 			break;
-			
+		case RIGHT: 
+			PILOT.rotate(10);
+			while (!isOnLine(RIGHT_SENSOR.readValue())) {
+				PILOT.rotateRight();
+			}
+			PILOT.rotate(10);
+			break;
+		case BACKWARD: 
+			PILOT.travel(-0.1);
+			PILOT.rotate(20);
+			while (!isOnLine(LEFT_SENSOR.readValue())) {
+				PILOT.rotateLeft();
+			}
+			PILOT.rotate(20);
+			break;
 		}
 	}
 	
 	public boolean isOnLine(int lightValue) {
-		return Math.abs(10 - lightValue) > 6;
+		return Math.abs(adjustmentValue - lightValue) > CORRECTION;
 	}
 
 }
