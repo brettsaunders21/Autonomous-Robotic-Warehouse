e4dc48676c30523146fd3c34105c2cee442af428
Brett James Saunders <bjs730@cs.bham.ac.uk>
2018-03-04 16:14:09 +0000

    Added object classes for Items and Jobs

---
+++ src-pc/jobs/Item.java
@@ -0,0 +1,57 @@
+import lejos.geom.Point;
+
+/**
+ * @author Brett Saunders <bjs730@cs.bham.ac.uk>
+ *
+ */
+public class Item {
+
+	private final String ID;
+	private final float WEIGHT;
+	private final float REWARD;
+	private final Point POSITION;
+	private final int QUANTITY;
+	
+	public Item(String _ID, int _QUANTITY) {
+		this.ID = _ID;
+		this.QUANTITY = _QUANTITY;
+		WEIGHT = 0;
+		POSITION = new Point(1,1);
+		REWARD = 0;
+	}
+
+	/**
+	 * @return the ID of the item
+	 */
+	public final String getID() {
+		return ID;
+	}
+
+	/**
+	 * @return the weight of the item
+	 */
+	public final float getWEIGHT() {
+		return WEIGHT;
+	}
+
+	/**
+	 * @return the reward of the item
+	 */
+	public final float getREWARD() {
+		return REWARD;
+	}
+
+	/**
+	 * @return the position of where to get the item
+	 */
+	public final Point getPOSITION() {
+		return POSITION;
+	}
+
+	/**
+	 * @return the quantity of item to pick up
+	 */
+	public final int getQUANTITY() {
+		return QUANTITY;
+	}
+}
