30b8aab956a57f617b375ad5a0244b62fdf50818
Brett James Saunders <bjs730@cs.bham.ac.uk>
2018-03-05 17:03:14 +0000

    Finished basic code for Robot controller and movement

--- src-robot/movement/Movement.java
+++ src-robot/movement/Movement.java
@@ -9,7 +9,6 @@ public class Movement {
 	private final LightSensor RIGHT_SENSOR;
 	private final DifferentialPilot PILOT;
 	private final int CORRECTION = 7;
-	private Configuration c = new Configuration();
 	
 	public Movement(int _adjustmentValue) {
 		PILOT = new WheeledRobotSystem(Configuration.CUSTOM_EXPRESS_BOT).getPilot();
@@ -18,8 +17,8 @@ public class Movement {
 		adjustmentValue = _adjustmentValue;
 	}
 	
-	public void nextAction(Action a) {
-		switch(a) {
+	public void nextAction(Action command, int pickAmount) {
+		switch(command) {
 		case WAIT: 
 			Delay.msDelay(100);
 			return;
@@ -46,6 +45,25 @@ public class Movement {
 				PILOT.rotateLeft();
 			}
 			PILOT.rotate(20);
+			break;
+		case PICKUP:
+			//Need interface code
+			break;
+		case DROPOFF:
+			//Need interface code
+			break;
+		case TURN_180: 
+			PILOT.rotate(180);
+			break;
+		case TURN_LEFT:
+			PILOT.rotate(90);
+			break;
+		case TURN_RIGHT:
+			PILOT.rotate(-90);
+			break;
+		case CANCEL:
+			break;
+		case SHUTDOWN:
 			break;
 		}
 	}
--- src-robot/robot/RobotController.java
+++ src-robot/robot/RobotController.java
@@ -1,4 +1,69 @@
+import lejos.nxt.LightSensor;
+import lejos.util.Delay;
+import rp.systems.StoppableRunnable;
 
-public class RobotController {
+public class RobotController implements StoppableRunnable{
+	private Movement move;
+	private  final LightSensor LEFT_SENSOR;
+	private final LightSensor RIGHT_SENSOR;
+	private boolean running;
+	
+	public RobotController() {
+		LEFT_SENSOR = new LightSensor(Configuration.LEFT_LIGHT_SENSOR);
+		RIGHT_SENSOR = new LightSensor(Configuration.RIGHT_LIGHT_SENSOR);
+		move = new Movement(calibrate());
+		running = true;
+	}
+
+	@Override
+	public void run() {
+		// TODO Start connection
+		Action currentCommand = null;
+		int pickAmount = 0;
+		while (running) {
+			//Print message
+			currentCommand = Action.valueOf(receiveCommand());
+			if (currentCommand != null) {
+				//Print Message
+				if (currentCommand.equals(Action.PICKUP)) {
+					pickAmount = receiveAmmount();
+				}
+				move.nextAction(currentCommand, pickAmount);
+			} else {
+				System.out.println("Error: No command received");
+				break;
+			}
+		}
+		sendCommand("ACTION COMPLETE");
+	}
+
+	@Override
+	public void stop() {
+		running = false;
+		
+	}
+	
+	public String receiveCommand() {
+		//Replace with bluetooth code
+		return "FORWARD";
+	}
+	
+	public int receiveAmmount() {
+		return 3;
+	}
+	
+	public void sendCommand(String command) {
+		//Replace with bluetooth code
+	}
+	
+	public int calibrate() {
+		Delay.msDelay(500);
+		return (LEFT_SENSOR.readValue() + RIGHT_SENSOR.readValue()) / 2;
+	}
+	
+	public static void main(String[] args) {
+		RobotController rc = new RobotController();
+		rc.run();
+	}
 
 }
