0c6d392d0a11e2fff8b3ffb05b82fd9577e0ffdc
dominikrys <dominik.rys@hotmail.com>
2018-03-06 20:45:34 +0000

    Split communication code to shared and started integrating

--- robot/src/communication/RobotNetworkHandler.java
+++ robot/src/communication/RobotNetworkHandler.java
@@ -1,24 +1,12 @@
 package communication;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import communication.AbstractSenderReceiver;
+
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
 
-public class RobotNetworkHandler implements Runnable {
-
-	private DataOutputStream outputStream;
-	private DataInputStream inputStream;
+public class RobotNetworkHandler extends AbstractSenderReceiver {
 	private BTConnection bluetoothConnection;
-	private boolean connected;
-
-	/*
-	 * The constructor
-	 */
-	public RobotNetworkHandler() {
-		connected = false;
-	}
 
 	@Override
 	public void run() {
@@ -27,47 +15,6 @@ public class RobotNetworkHandler implements Runnable {
 		bluetoothConnection = Bluetooth.waitForConnection();
 		outputStream = bluetoothConnection.openDataOutputStream();
 		inputStream = bluetoothConnection.openDataInputStream();
-		connected = true;
 		System.out.println("Bluetooth connection established! Receiver and sender streams created");
-	}
-
-	public boolean isConnected() {
-		return connected;
-	}
-
-	/*
-	 * Method for receiving objects from PC
-	 */
-	public Object receiveObject(Object inputObject) throws IOException {
-		// Read the right kind of data dependent on the format of the input - most will
-		// be handled by the enum or integer, the others included for redundancy
-		if (inputObject instanceof String || inputObject instanceof Enum) {
-			return inputStream.readUTF();
-		} else if (inputObject instanceof Integer) {
-			return inputStream.readInt();
-		} else if (inputObject instanceof Double) {
-			return inputStream.readDouble();
-		} else if (inputObject instanceof Float) {
-			return inputStream.readFloat();
-		}
-		
-		return null;
-	}
-
-	/*
-	 * Method for sending objects to PC
-	 */
-	public void sendObject(Object inputObject) throws IOException {
-		if (inputObject instanceof Integer || inputObject instanceof Enum) {
-			outputStream.writeInt((int) inputObject);
-		} else if (inputObject instanceof String) {
-			outputStream.writeUTF((String) inputObject);
-		} else if (inputObject instanceof Double) {
-			outputStream.writeDouble((double) inputObject);
-		} else if (inputObject instanceof Float) {
-			outputStream.writeFloat((float) inputObject);
-		}
-		
-		outputStream.flush();
 	}
 }
