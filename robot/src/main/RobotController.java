3229d33f6bffda11fbde040da173ce91b0552d73
Brett James Saunders <bjs730@cs.bham.ac.uk>
2018-03-07 00:22:17 +0000

    Fixed Controller

--- robot/src/main/RobotController.java
+++ robot/src/main/RobotController.java
@@ -3,8 +3,8 @@ package main;
 import java.io.IOException;
 
 import actions.Movement;
-import communication.RobotNetworkHandler;
 import communication.CommunicationData;
+import communication.RobotNetworkHandler;
 import interfaces.Action;
 import lejos.nxt.LightSensor;
 import lejos.util.Delay;
@@ -32,45 +32,18 @@ public class RobotController implements StoppableRunnable{
 		int pickAmount = 0;
 		while (running) {
 			//Print message
-			try {
-				currentCommand = (Action) networkHandler.receiveObject(CommunicationData.ACTION);
-				switch (currentCommand) {
-				case LEFT:
-					break;
-				case FORWARD:
-					break;
-				case RIGHT:
-					break;
-				case BACKWARD:
-					break;
-				case WAIT:
-					break;
-				case PICKUP:
+			currentCommand = (Action) networkHandler.receiveObject(CommunicationData.ACTION);
+			if (currentCommand != null) {
+				//Print Message
+				if (currentCommand.equals(Action.PICKUP)) {
 					pickAmount = (int) networkHandler.receiveObject(CommunicationData.INT);
-					move.nextAction(currentCommand, pickAmount);
-					break;
-				case DROPOFF:
-					break;
-				case TURN_180:
-					break;
-				case TURN_LEFT:
-					break;
-				case TURN_RIGHT:
-					break;
-				case CANCEL:
-					break;
-				case SHUTDOWN:
-					break;
-				default:
-					System.out.println("Error: No valid command received");
-					break;
-				
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
+				move.nextAction(currentCommand, pickAmount);
+			} else {
+				System.out.println("Error: No command received");
+				break;
 			}
 		}
-		
 		try {
 			networkHandler.sendObject("ACTION COMPLETE");
 		} catch (IOException e) {
