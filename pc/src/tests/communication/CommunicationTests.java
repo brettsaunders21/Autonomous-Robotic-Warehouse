630fb4d8e94882624d6e057130bde1fd4b0e9c43
dominikrys <dominik.rys@hotmail.com>
2018-03-10 19:38:59 +0000

    Change method of connection from NXTComm to nxtconnection to allow retries, modified communication JUnit tests and added log4j properties file

--- pc/src/tests/communication/CommunicationTests.java
+++ pc/src/tests/communication/CommunicationTests.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 import lejos.pc.comm.NXTCommFactory;
 import lejos.pc.comm.NXTInfo;
@@ -20,94 +22,93 @@ import interfaces.Action;
  * CommunicationTestHelper on the robot for there to work.
  */
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CommunicationTests {
-	
-	NXTInfo nxtInfo;
-	PCNetworkHandler networkHandler;
+
+	static NXTInfo nxtInfo;
+	static PCNetworkHandler networkHandler;
 
 	// Set up the bluetooth connection before any tests
 	@BeforeClass
-	public void setup() {
+	public static void setup() {
 		// Drawer A3 - Spike - 0016530AA681
-		nxtInfo.deviceAddress = "0016530AA681";
-		nxtInfo.protocol = NXTCommFactory.BLUETOOTH;
-		nxtInfo.name = "Spike";
-		
+		nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "Spike", "0016530AA681");
+
 		networkHandler = new PCNetworkHandler(nxtInfo);
 	}
-	
+
 	// Test if a connection can be established
-	@Test(timeout=1000)
-	public void singleRobotConnectionTest() throws InterruptedException{
+	@Test(timeout = 10000)
+	public void singleRobotConnectionTest() throws InterruptedException {
 		networkHandler.run();
 		Thread.sleep(5000);
 		assertTrue(networkHandler.isConnected());
 	}
-	
+
 	// Test if an Action object can be sent and received
-	@Test(timeout=1000)
+	@Test(timeout = 5000)
 	public void singleRobotSendReceiveActionTest() throws IOException, InterruptedException {
 		Action testObject = Action.WAIT;
 		networkHandler.sendObject(testObject);
-		
+
 		Thread.sleep(1000);
-		
+
 		Action receivedObject = (Action) networkHandler.receiveObject(CommunicationData.ACTION);
-		
+
 		assertEquals(testObject, receivedObject);
 	}
-	
+
+	// Test if a double can be sent and received
+	@Test(timeout = 5000)
+	public void singleRobotSendReceiveDoubleTest() throws IOException, InterruptedException {
+		double testObject = 0.97338474f;
+		networkHandler.sendObject(testObject);
+
+		Thread.sleep(1000);
+
+		double receivedObject = (double) networkHandler.receiveObject(CommunicationData.DOUBLE);
+
+		assertEquals(testObject, receivedObject, 0.5f);
+	}
+
+	// Test if a float can be sent and received
+	@Test(timeout = 5000)
+	public void singleRobotSendReceiveFloatTest() throws IOException, InterruptedException {
+		float testObject = 0.56443f;
+		networkHandler.sendObject(testObject);
+
+		Thread.sleep(1000);
+
+		float receivedObject = (float) networkHandler.receiveObject(CommunicationData.FLOAT);
+
+		assertEquals(testObject, receivedObject, 0.5f);
+	}
+
 	// Test if an integer can be sent and received
-	@Test(timeout=1000)
+	@Test(timeout = 5000)
 	public void singleRobotSendReceiveIntegerTest() throws IOException, InterruptedException {
 		int testObject = 43565;
 		networkHandler.sendObject(testObject);
-		
+
 		Thread.sleep(1000);
-		
+
 		int receivedObject = (int) networkHandler.receiveObject(CommunicationData.INT);
-		
+
 		assertEquals(testObject, receivedObject);
 	}
-	
+
 	// Test if a string can be sent and received
-	@Test(timeout=1000)
+	@Test(timeout = 5000)
 	public void singleRobotSendReceiveStringTest() throws IOException, InterruptedException {
 		String testObject = "Test string";
 		networkHandler.sendObject(testObject);
-		
+
 		Thread.sleep(1000);
-		
+
 		String receivedObject = (String) networkHandler.receiveObject(CommunicationData.STRING);
-		
+
 		assertEquals(testObject, receivedObject);
 	}
-	
-	// Test if a float can be sent and received
-		@Test(timeout=1000)
-		public void singleRobotSendReceiveFloatTest() throws IOException, InterruptedException {
-			float testObject = 0.56443f;
-			networkHandler.sendObject(testObject);
-			
-			Thread.sleep(1000);
-			
-			float receivedObject = (float) networkHandler.receiveObject(CommunicationData.FLOAT);
-			
-			assertEquals(testObject, receivedObject, 0.5f);
-		}
-		
-		// Test if a double can be sent and received
-		@Test(timeout=1000)
-		public void singleRobotSendReceiveDoubleTest() throws IOException, InterruptedException {
-			double testObject = 0.97338474f;
-			networkHandler.sendObject(testObject);
-			
-			Thread.sleep(1000);
-			
-			double receivedObject = (double) networkHandler.receiveObject(CommunicationData.DOUBLE);
-			
-			assertEquals(testObject, receivedObject, 0.5f);
-		}
-	
+
 	// Include more tests + multiple robot tests?
 }
