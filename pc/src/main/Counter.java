42eab16e195c9affdb6d791b540c97cf6d996c9b
ben <bensassoon@gmail.com>
2018-03-18 12:41:19 +0000

    Trying to fix threads

--- pc/src/main/Counter.java
+++ pc/src/main/Counter.java
@@ -1,26 +1,34 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+
+import java.util.List;
 
 import interfaces.Robot;
 
 public class Counter {
 	private int time = 0;
-	private boolean[] readyToMove;
+	//private List<Boolean> readyToMove;
 	private ConcurrentMap<String, Integer> nameIndex = new ConcurrentHashMap<String, Integer>();
+	private List<Boolean> readyToMove;
 	
 	public Counter(Robot[] ROBOTS) {
-		readyToMove = new boolean[ROBOTS.length];
+		//readyToMove = new boolean[ROBOTS.length];
+		readyToMove = Collections.synchronizedList(new ArrayList<Boolean>());
 		int i=0;
 		for (Robot robot : ROBOTS) {
-			readyToMove[i] = false;
-			nameIndex.put(robot.getRobotName(), i++);
+			readyToMove.add(i,false);
+			nameIndex.put(robot.getRobotName(), i);
+			i++;
 		}
 	}
 	
 	public synchronized void readyToMove(String robotName) {
-		readyToMove[nameIndex.get(robotName)] = true;
+		//readyToMove[nameIndex.get(robotName)] = true;
+		readyToMove.set(nameIndex.get(robotName), true);
 	}
 	
 	public synchronized boolean canMove() {
@@ -32,8 +40,9 @@ public class Counter {
 		}
 		if (flag) {
 			time++;
-			for (int i = 0; i<readyToMove.length; i++) {
-				readyToMove[i] = false;
+			for (int i = 0; i<readyToMove.size(); i++) {
+				//readyToMove[i] = false;
+				readyToMove.set(i, false);
 			}
 		}
 		return flag;
