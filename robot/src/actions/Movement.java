ecc0ea4c4b66767a2c0001097ee9f12d48738d51
dominikrys <dominik.rys@hotmail.com>
2018-03-21 01:26:46 +0000

    Most recent version, doesn't really work

--- robot/src/actions/Movement.java
+++ robot/src/actions/Movement.java
@@ -9,14 +9,13 @@ import main.Configuration;
 
 public class Movement {
 	private final int MID_BOUND;
-	private  final LightSensor LEFT_SENSOR;
+	private final LightSensor LEFT_SENSOR;
 	private final LightSensor RIGHT_SENSOR;
 	private final DifferentialPilot PILOT;
 	private RobotInterface rInterface;
 	private final float DEGREETURN = 90f;
 	private final float HALFLENGTH = 24.5f;
-	
-	
+
 	public Movement(int _MID_BOUND, RobotInterface _rI) {
 		PILOT = new DifferentialPilot(56, 110.5, Motor.A, Motor.C);
 		LEFT_SENSOR = new LightSensor(Configuration.LEFT_LIGHT_SENSOR);
@@ -26,13 +25,13 @@ public class Movement {
 		PILOT.setRotateSpeed(PILOT.getRotateMaxSpeed());
 		rInterface = _rI;
 	}
-	
+
 	public void nextAction(Action command, int pickAmount) {
-		switch(command) {
+		switch (command) {
 		case HOLD:
 			Delay.msDelay(100);
 			return;
-		case WAIT: 
+		case WAIT:
 			Delay.msDelay(100);
 			return;
 		case FORWARD:
@@ -42,15 +41,15 @@ public class Movement {
 			PILOT.rotate(-DEGREETURN);
 			PILOT.forward();
 			break;
-		case RIGHT: 
+		case RIGHT:
 			PILOT.travel(HALFLENGTH);
 			PILOT.rotate(DEGREETURN);
 			PILOT.forward();
 			break;
-		case BACKWARD: 
+		case BACKWARD:
 			PILOT.stop();
 			PILOT.travel(-HALFLENGTH);
-			PILOT.rotate(DEGREETURN*2);
+			PILOT.rotate(DEGREETURN * 2);
 			PILOT.forward();
 			break;
 		case PICKUP:
@@ -68,24 +67,25 @@ public class Movement {
 		default:
 			break;
 		}
-		if (!(command.equals(Action.PICKUP) || command.equals(Action.DROPOFF)|| command.equals(Action.WAIT)|| command.equals(Action.HOLD))) {
+		if (!(command.equals(Action.PICKUP) || command.equals(Action.DROPOFF) || command.equals(Action.WAIT)
+				|| command.equals(Action.HOLD))) {
 			while (!(isRightOnLine() && isLeftOnLine())) {
-			PILOT.forward();
-			if (isLeftOnLine() && !isRightOnLine()) {
-				Motor.C.setSpeed(Motor.C.getSpeed() * 0.70f);
-				Delay.msDelay(20);
+				PILOT.forward();
+				if (isLeftOnLine() && !isRightOnLine()) {
+					Motor.C.setSpeed(Motor.C.getSpeed() * 0.1f);
+					Delay.msDelay(40);
+				}
+				if (!isLeftOnLine() && isRightOnLine()) {
+					Motor.A.setSpeed(Motor.A.getSpeed() * 0.1f);
+					Delay.msDelay(40);
+				}
+				PILOT.setRotateSpeed(PILOT.getRotateMaxSpeed());
 			}
-			if  (!isLeftOnLine() && isRightOnLine()) {
-				Motor.A.setSpeed(Motor.A.getSpeed() * 0.70f);
-				Delay.msDelay(20);
-			}
-			PILOT.setRotateSpeed(PILOT.getRotateMaxSpeed());
-		}
 		}
 		PILOT.stop();
 		rInterface.resetQuantity();
 	}
-	
+
 	public boolean isOnLine(int lightValue) {
 		if (lightValue <= MID_BOUND) {
 			return true;
@@ -93,11 +93,11 @@ public class Movement {
 			return false;
 		}
 	}
-	
+
 	public boolean isRightOnLine() {
 		return isOnLine(RIGHT_SENSOR.getNormalizedLightValue());
 	}
-	
+
 	public boolean isLeftOnLine() {
 		return isOnLine(LEFT_SENSOR.getNormalizedLightValue());
 	}
